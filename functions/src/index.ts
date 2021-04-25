3ba241290ec083ccb97689badc34524b40ce4981
Takamasa Arakawa <takamasa_arakawa@metaps.com>
2021-04-25 23:50:52 +0900

    change restriction spec

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,8 @@ import { Util } from './services/util';
 //     $ curl 'http://localhost:5000/replica-nft-api/us-central1/mint?message={<contractAddress>-<tokenId>-<owner>-<flowOwner>-<expiresUnixtime>}&signature={signature}'
 //       e.g. curl 'http://localhost:5000/replica-nft-api/us-central1/mint?message=0xd07dc4262bcdbf85190c01c996b4c06a461d2430-128695-0x98d562c7A4781e3e6c0d16F67469b0A3b0CB25C7-0xf8d6e0586b0a20c7-4102434800&signature=0x171f50b4ae206867be24fce0128504205f0122f129cdf3e9107a51004aeec94f5184477effce4d3fed7b6e377f781dce4dfe4323f4b4f65a89f356321f8c8bbd1b'
 
+const util = new Util(config);
+
 const getParams = (req: any) => {
   const { message, signature } = req.query;
   const [ contractAddress, tokenId, owner, flowOwner, expiresAt ] = message.split('-');
@@ -21,10 +23,12 @@ const getParams = (req: any) => {
 
 const validate = async (req: any) => {
   try {
-    const { contractAddress, tokenId, owner, expiresAt, message, signature } = getParams(req);
+    const { contractAddress, tokenId, owner, flowOwner, expiresAt, message, signature } = getParams(req);
     if (expiresAt < Math.floor(Date.now() / 1000)) throw new Error('Expired');
     await opensea.validateOwner(contractAddress, tokenId, owner);
     ethUtil.validateSignature(message, signature, owner);
+    if (!(await util.executeScript('check_account.cdc', [[ flowOwner, t.Address ]]))) throw new Error('Account Not Initialized');
+    if (await util.executeScript('exists_replica.cdc', [[ `${contractAddress.toLowerCase()}-${tokenId}`, t.String ]])) throw new Error('Already Minted');
     return { success: true };
   } catch (e) {
     return { error: e.message };
@@ -37,12 +41,11 @@ const validateAndMint = async (req: any) => {
     if (!validateResult.success) return validateResult;
 
     const { contractAddress, tokenId, owner, flowOwner, message, signature } = getParams(req);
-    const util = new Util(config);
     const res = await util.sendTx('mint_nft.cdc', [
       [ flowOwner, t.Address ], // recipient
-      [ contractAddress, t.String ], // contractAddress
+      [ contractAddress.toLowerCase(), t.String ], // contractAddress
       [ Number(tokenId), t.UInt64 ], // tokenId
-      [ owner, t.String ], // ownerAtTime
+      [ owner.toLowerCase(), t.String ], // ownerAtTime
       [ message, t.String ], // message
       [ signature, t.String ], // signature
     ]);
